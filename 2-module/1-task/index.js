c355de36d3d420b0804f3e3b2af3398441be4abd
vovandrelo <vovandrelo@gmail.com>
2022-04-21 21:02:54 +0300

    Mod 2, tasks 1-3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+    for (let key in salaries) {
+      const salary = salaries[key];
+      if (typeof(salary) === "number") {
+        if (!isNaN(salary) && salary !== -Infinity && salary !== +Infinity) {
+          sum += salary;
+        }
+      }
+    }
+  return sum;
 }

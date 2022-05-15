3e093c9f3d36452e552933fb385650f19ab34aaf
vovandrelo <vovandrelo@gmail.com>
2022-05-15 18:54:21 +0300

    Mod 4, tasks 1-3; Mod 5, tasks 1-3. Modification

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -4,7 +4,7 @@ function initCarousel() {
   const arrowRight = carousel.querySelector(".carousel__arrow_right");
   const carouselInner = carousel.querySelector(".carousel__inner");
 
-  const widthSlide = carousel.offsetWidth;
+  const widthSlide = carouselInner.offsetWidth;
   let position = 0;
   const numSlides = carouselInner.querySelectorAll(".carousel__slide").length;
   let curSlide = 1;
@@ -18,19 +18,24 @@ function initCarousel() {
   //=== ! сдвигает  слайд  лишь  на  половину.  Сдвиг  через  "offsetWidth" ! ===\\
   //=== ! сдвигает   слайд   полностью,   но   не   проходит   через  тесты ! ===\\
 
+  //=== ! Ответ: "Попробуй взять carouselInner.offsetWidth" ! ===\\
+
+  //=== ! Да,  действительно,  проблема  была  только в этом... Я почему то ! ===\\
+  //=== ! думал что  offsetWidth предоставляет полную ширину элемента, а не ! ===\\
+  //=== ! его видимую часть... Но, изучив документацию, оказалось наоборот) ! ===\\
+  //=== ! Теперь всё работает корректно                                     ! ===\\
+
   arrowLeft.addEventListener("click", () => {
     curSlide--;
     arrowControl(curSlide, numSlides, arrowLeft, arrowRight);
-    // position += widthSlide;
-    position += 500;
+    position += widthSlide;
     carouselInner.style.transform = `translateX(${position}px)`;
   });
 
   arrowRight.addEventListener("click", () => {
     curSlide++;
     arrowControl(curSlide, numSlides, arrowLeft, arrowRight);
-    // position -= widthSlide;
-    position -= 500;
+    position -= widthSlide;
     carouselInner.style.transform = `translateX(${position}px)`;
   });
 }
